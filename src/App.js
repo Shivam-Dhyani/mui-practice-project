5fe5b3850a36eaf8334e0c69b9a372a0437a1532
Shivam-Dhyani <shivamdhiyani15@gmail.com>
2023-02-17 18:00:12 +0530

    project started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography } from "@mui/material";
+import { Add, Delete } from "@mui/icons-material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MUI Learning App</h1>
+      <Button startIcon={<Delete />} color="error" variant="contained">
+        Delete Post
+      </Button>
+      <Button variant="contained">Post</Button>
+      <Button startIcon={<Add />} color="success" variant="contained">
+        Add New Post
+      </Button>
+      <Typography></Typography>
     </div>
   );
 }
