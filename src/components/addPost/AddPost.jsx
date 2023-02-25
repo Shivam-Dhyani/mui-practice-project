b559bdcce78fdd8339e7d10227c33821f29c8a3d
Shivam Dhyani <shivamdhiyani15@gmail.com>
2023-02-25 05:41:19 +0530

    AddPost Componenet Done!!

--- src/components/addPost/AddPost.jsx
+++ src/components/addPost/AddPost.jsx
@@ -1,19 +1,39 @@
-import { Add } from "@mui/icons-material";
 import {
+  Add,
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from "@mui/icons-material";
+import {
+  Avatar,
   Box,
+  Button,
+  ButtonGroup,
   Fab,
   IconButton,
   Modal,
+  Stack,
   styled,
+  TextField,
   Tooltip,
   Typography,
 } from "@mui/material";
+
 import React, { useState } from "react";
 
 const StyledModal = styled(Modal)({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
 });
 
 const AddPost = () => {
@@ -45,6 +65,37 @@ const AddPost = () => {
           <Typography variant="h6" color="grey" textAlign="center">
             Create Post
           </Typography>
+          <UserBox>
+            <Avatar
+              sx={{ width: "40px", height: "40px" }}
+              src="https://mui.com/static/images/avatar/6.jpg"
+            />
+            <Typography>John</Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            multiline
+            rows={3}
+            placeholder="What's in your Mind?"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
+          <ButtonGroup
+            variant="contained"
+            fullWidth
+            aria-label="outlined primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "100% !important" }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
         </Box>
       </StyledModal>
     </>
